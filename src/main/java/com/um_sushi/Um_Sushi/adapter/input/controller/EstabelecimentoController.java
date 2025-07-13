da33f921ab1ecdb399bbb44896479d4a2e800fae
Jean <jeamsillva3@gmail.com>
2025-07-13 17:28:45 -0300

    :sparkles: feat: Conclui o fluxo de finalização de pagamento e adiciona validações de DTOs

--- src/main/java/com/um_sushi/Um_Sushi/adapter/input/controller/EstabelecimentoController.java
+++ src/main/java/com/um_sushi/Um_Sushi/adapter/input/controller/EstabelecimentoController.java
@@ -1,12 +1,12 @@
 package com.um_sushi.Um_Sushi.adapter.input.controller;
 
-
 import com.um_sushi.Um_Sushi.adapter.input.dto.requets.PedidoRequest;
 import com.um_sushi.Um_Sushi.adapter.input.dto.responses.PedidoResponse;
 import com.um_sushi.Um_Sushi.adapter.input.dto.responses.ProdutoResponse;
 import com.um_sushi.Um_Sushi.adapter.input.mapper.EstabelecimentoMapper;
 import com.um_sushi.Um_Sushi.port.input.ConsultarEstabelecimentoUserCase;
 import com.um_sushi.Um_Sushi.port.input.SalvarEstabelecimentoUserCase;
+import jakarta.validation.Valid; // Adicionar este import
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
@@ -31,7 +31,7 @@ public class EstabelecimentoController {
     private final EstabelecimentoMapper mapper;
 
     @PostMapping("/pedido")
-    public ResponseEntity<PedidoResponse> salvarPedido(@RequestBody PedidoRequest request) {
+    public ResponseEntity<PedidoResponse> salvarPedido(@Valid @RequestBody PedidoRequest request) {
         LOGGER.info("Fazendo pedido: " + request);
 
         String id = salvarEstabelecimentoUserCase
